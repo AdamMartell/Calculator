8a2b66547a9abff9a9bc19a1585f66f978996297
HobbitMafia <volleyman12@gmail.com>
2015-09-24 08:43:35 -0500

    test

---
+++ CalculatorTest/CalculatorTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void TestAdd()
+        {
+            MathsCalculator calculator = new MathsCalculator();
+            decimal expected = 10;
+            decimal actual = calculator.Add(5,5);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
